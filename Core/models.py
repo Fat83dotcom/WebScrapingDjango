85b16eb4d48a2104fe6240eb1a104db6b7db79ce
fat83dotcom <fernando.quimic@gmail.com>
2022-10-22 10:49:21 -0300

    Desenvolvimento da aplicação - views - templates - banco de dados

--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,32 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Materiasportal(models.Model):
+    id_pk = models.IntegerField(primary_key=True)
+    referencia_site = models.ForeignKey('Portalcnn', models.DO_NOTHING, db_column='referencia_site', blank=True, null=True)
+    dt_materia = models.DateField(blank=True, null=True)
+    link_materia = models.TextField(blank=True, null=True)
+    titulo_materia = models.TextField(blank=True, null=True)
+    texto_materia = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'materiasportal'
+
+
+class Portalcnn(models.Model):
+    id_pk = models.IntegerField(primary_key=True)
+    dt_hr_pesquisa = models.DateTimeField(blank=True, null=True)
+    nome_sessao = models.CharField(max_length=100, blank=True, null=True)
+    link_site = models.CharField(unique=True, max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'portalcnn'
--- Core/views.py
+++ Core/views.py
@@ -1,79 +1,26 @@
 from datetime import datetime
 from django.shortcuts import render
-from bs4 import BeautifulSoup
-import requests
-from itertools import count
-from datetime import datetime
-from time import sleep
-
+from .models import Materiasportal, Portalcnn
+from django.db import connection
 
 def index(request):
-    return render(request, 'Core/index.html')
+    consultaSQL = Portalcnn.objects.get(pk=0)
+    return render(request, 'Core/index.html', {
+        'dtAtualizacao': consultaSQL,
+    })
 
+
+# def consultaPortaCnn():
+#     with connection.cursor() as cursor:
+#         sql = 
 
 def coreCnn(request):
-    with open('Core/templates/Core/noticiasCnn.html', 'w') as arquivo:
-        url = [
-            'https://www.cnnbrasil.com.br/politica/',
-            'https://www.cnnbrasil.com.br/nacional/',
-            'https://www.cnnbrasil.com.br/business/',
-            'https://www.cnnbrasil.com.br/internacional/',
-            'https://www.cnnbrasil.com.br/esporte/',
-            'https://www.cnnbrasil.com.br/saude/',
-            'https://www.cnnbrasil.com.br/tecnologia/',
-            'https://www.cnnbrasil.com.br/entretenimento/',
-            'https://www.cnnbrasil.com.br/estilo/',
-            'https://www.cnnbrasil.com.br/loterias/',
-        ]
-        arquivo.write(
-            '''
-{%extends 'base.html'%}
-{%block conteudo%}
-<main>    
-    '''
-        )
-        for links in url:
-            resposta = requests.get(links)
-            html = BeautifulSoup(resposta.text, 'html.parser')
-            arquivo.write(
-                '''
-    <div class="geral">
-        <a href="#https://www.cnnbrasil.com.br/politica/">Política</a>
-        <a href="#https://www.cnnbrasil.com.br/nacional/">Nacional</a>
-        <a href="#https://www.cnnbrasil.com.br/business/">Business</a>
-        <a href="#https://www.cnnbrasil.com.br/internacional/">Internacional</a>
-        <a href="#https://www.cnnbrasil.com.br/esporte/">Esporte</a>
-        <a href="#https://www.cnnbrasil.com.br/saude/">Saude</a>
-        <a href="#https://www.cnnbrasil.com.br/tecnologia/">Tecnologia</a>
-        <a href="#https://www.cnnbrasil.com.br/entretenimento/">Entretenimento</a>
-        <a href="#https://www.cnnbrasil.com.br/estilo/">Estilo</a>
-        <a href="#https://www.cnnbrasil.com.br/loterias/">Loterias</a>
-    </div>
-                '''
-            )
-            arquivo.write(f'<div class="titulo"><h1 id="{links}">Seção do Site:</h1> <a href="{links}" target="_blanck">'
-            f'{links}</a> </div>')
-            contador = count(1)
-            for noticias in html.select('.home__list__item'):
-                tituloMateria = noticias.a.get_text()
-                linkMateria = noticias.a.get('href')
-                arquivo.write(f'<div class="titulo"> <h2>Nº {next(contador)} | Horario do Scraping: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
-                f' </h2> '
-                f'<h3>{tituloMateria}</h3> <a href="{linkMateria}" target="_blanck">Link Materia</a> ')
-                resp = requests.get(linkMateria)
-                html1 = BeautifulSoup(resp.text, 'html.parser')
-                for materia in html1.select('.posts'):
-                    dataMateria = materia.select_one('.post__data').get_text(strip=True)
-                    texto = materia.select_one('.post__content').get_text(' | ', strip=True)
-                    arquivo.write(f'<h3>Data Matéria: {dataMateria}</h3> </div>')
-                    if texto is None:
-                        arquivo.write('<div class="geral"> <p>Sem texto ...</p> </div>')
-                    else:
-                        arquivo.write(f'<div class="geral"> <p>{texto}</p> </div>')
-        arquivo.write(
-'''</main>
-{%endblock%}'''
-        )
-
-    # sleep(10)    
-    return render(request, 'Core/noticiasCnn.html')
+    if request.method != 'GET':
+        return render(request, 'Core/noticiasCnn.html')
+    else:
+        consultaSQL = Portalcnn.objects.prefetch_related('referencia_site')
+        # print(consultaSQL.get(referencia_site_id=0))
+        contexto = {
+            'dados': consultaSQL,
+        }
+        return render(request, 'Core/noticiasCnn.html', contexto)
